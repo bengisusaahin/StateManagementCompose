396f38cfc69b755ef700fe71c9d9f10e50c315b0
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-01-03 22:30:50 +0300

    other composables

--- app/src/main/java/com/bengisusahin/statemanagementcompose/MainActivity.kt
+++ app/src/main/java/com/bengisusahin/statemanagementcompose/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.bengisusahin.statemanagementcompose
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContent {
+            MainScreen()
+        }
+
     }
+}
+
+@Composable
+fun MainScreen(){
+    Surface(color = Color.LightGray) {
+        Column(modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Text(text = "Hello!",
+                fontSize = 26.sp
+            )
+            Spacer(modifier = Modifier.padding(7.dp))
+            Button(onClick = {
+                println("clicked")
+            }) {
+                Text(text = "Button")
+                Text(text = "Test")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview(){
+    MainScreen()
 }
