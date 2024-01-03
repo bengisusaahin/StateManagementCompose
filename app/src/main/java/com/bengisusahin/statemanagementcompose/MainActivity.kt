72396d890bb8d9f864cd6b1e8afb80379be1e11d
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-01-04 00:26:55 +0300

    image

--- app/src/main/java/com/bengisusahin/statemanagementcompose/MainActivity.kt
+++ app/src/main/java/com/bengisusahin/statemanagementcompose/MainActivity.kt
@@ -3,11 +3,13 @@ package com.bengisusahin.statemanagementcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -15,6 +17,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -47,6 +55,23 @@ fun MainScreen(){
                 Text(text = "Button")
                 Text(text = "Test")
             }
+            Spacer(modifier = Modifier.padding(7.dp))
+
+            Image(bitmap = ImageBitmap.imageResource(id = R.drawable.metallica),
+                contentDescription = "metallica",
+                modifier = Modifier.size(200.dp))
+            Spacer(modifier = Modifier.padding(7.dp))
+
+            Image(imageVector = ImageVector.vectorResource(
+                id = R.drawable.ic_launcher_foreground),
+                contentDescription = null )
+            Spacer(modifier = Modifier.padding(7.dp))
+
+            Image(painter = ColorPainter(Color.Black),
+                contentDescription = null,
+                modifier = Modifier.size(20.dp,20.dp)
+                )
+
         }
     }
 }
