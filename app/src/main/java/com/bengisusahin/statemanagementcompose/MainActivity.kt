e5e639e884f46b3bbbef865acd772f1fb1b3566a
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-01-04 16:27:21 +0300

    state Hoisting

--- app/src/main/java/com/bengisusahin/statemanagementcompose/MainActivity.kt
+++ app/src/main/java/com/bengisusahin/statemanagementcompose/MainActivity.kt
@@ -3,14 +3,11 @@ package com.bengisusahin.statemanagementcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -19,14 +16,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.painter.ColorPainter
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.imageResource
-import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,6 +27,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
+            //OtherScreen()
             MainScreen()
         }
 
@@ -44,58 +36,41 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(){
-    Surface(color = Color.LightGray) {
-        Column(modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
+    Surface(modifier = Modifier.fillMaxSize()
+    ) {
+        Column(verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
-        ){
-            //var myString = "Android Compose"
-            var myString = remember{mutableStateOf("Android Compose")}
-
-            TextField(
-                value = myString.value,
-                onValueChange = {
-                    myString.value = it
-                }
-            )
-            Spacer(modifier = Modifier.padding(7.dp))
-
-            var textString = remember{ mutableStateOf("Hello!") }
-
-            Text(text = textString.value,
-                fontSize = 26.sp
-            )
-            Spacer(modifier = Modifier.padding(7.dp))
-            Button(onClick = {
-                textString.value = "Android"
-                //println(textString)
-            }) {
-                Text(text = "Button")
-                Text(text = "Test")
-            }
-            Spacer(modifier = Modifier.padding(7.dp))
-
-            Image(bitmap = ImageBitmap.imageResource(id = R.drawable.metallica),
-                contentDescription = "metallica",
-                modifier = Modifier.size(200.dp))
-            Spacer(modifier = Modifier.padding(7.dp))
-
-            Image(imageVector = ImageVector.vectorResource(
-                id = R.drawable.ic_launcher_foreground),
-                contentDescription = null )
-            Spacer(modifier = Modifier.padding(7.dp))
-
-            Image(painter = ColorPainter(Color.Black),
-                contentDescription = null,
-                modifier = Modifier.size(20.dp,20.dp)
-                )
-
+        ) {
+            SpecialText(string = "Test")
+            Spacer(modifier = Modifier.padding(5.dp))
+            SpecialText(string = "Android")
+            Spacer(modifier = Modifier.padding(5.dp))
+            SpecialTextField()
         }
     }
+
+}
+
+@Composable
+fun SpecialText(string: String){
+    Text(text = string,
+        fontSize = 26.sp,
+        fontStyle = FontStyle.Italic,
+        fontFamily = FontFamily.SansSerif
+    )
+}
+
+@Composable
+fun SpecialTextField(){
+    var myString = remember{ mutableStateOf("") }
+    TextField(value = myString.value, onValueChange = {
+        myString.value = it
+    } )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview(){
+    //OtherScreen()
     MainScreen()
 }
