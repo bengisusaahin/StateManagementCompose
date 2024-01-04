1dc2936e1df818c41e3ba69684f9ca287ff8df42
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-01-04 15:58:53 +0300

    recomposition

--- app/src/main/java/com/bengisusahin/statemanagementcompose/MainActivity.kt
+++ app/src/main/java/com/bengisusahin/statemanagementcompose/MainActivity.kt
@@ -15,6 +15,8 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -47,26 +49,26 @@ fun MainScreen(){
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ){
-            var myString = "Android Compose"
+            //var myString = "Android Compose"
+            var myString = remember{mutableStateOf("Android Compose")}
 
             TextField(
-                value = TextFieldValue(text = myString),
+                value = myString.value,
                 onValueChange = {
-                    myString = it.toString()
-                    println(myString)
+                    myString.value = it
                 }
             )
             Spacer(modifier = Modifier.padding(7.dp))
 
-            var textString = "Hello!"
+            var textString = remember{ mutableStateOf("Hello!") }
 
-            Text(text = textString,
+            Text(text = textString.value,
                 fontSize = 26.sp
             )
             Spacer(modifier = Modifier.padding(7.dp))
             Button(onClick = {
-                textString = "Android"
-                println(textString)
+                textString.value = "Android"
+                //println(textString)
             }) {
                 Text(text = "Button")
                 Text(text = "Test")
